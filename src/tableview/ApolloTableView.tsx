945f7615bad3ccbe9d4bfc193f63357b589b16ef
CXB0U4R <christopher.blakely@nexteraenergy.com>
2019-08-19 15:49:13 -0400

    Reduced extra renders, started implementing recursive column generation

--- src/tableview/ApolloTableView.tsx
+++ src/tableview/ApolloTableView.tsx
@@ -5,12 +5,13 @@ import { ApolloClient, gql } from "apollo-boost";
 import { useQuery } from "@apollo/react-hooks";
 import { ColumnProps } from "antd/lib/table";
 import { Omit } from "antd/es/_util/type";
-
+import { QueryResult } from "@apollo/react-common";
 
 interface ApolloColumnDefinition<T extends DomainEntity> {
   keyColumn: string; // unique row identifier always aliased as 'key'
   columns?: ColumnProps<T>[]; // all columns if undefined
   excludeColumns?: string[];
+  recursive?: boolean; //Flag to indicate whether columns should be generated recursively for subtypes
 }
 
 export interface ApolloTableViewProps<T extends DomainEntity>
@@ -29,6 +30,12 @@ function buildIntrospectionQuery(entityName: string) {
                 fields {
                   dataIndex:name
                   title:description
+                    type {
+                        fields {
+                            dataIndex: name
+                            title: description
+                        }
+                    }
                 }
               }
             }`;
@@ -38,13 +45,17 @@ export function ApolloTableView<T extends DomainEntity>(
   props: ApolloTableViewProps<T>
 ) {
   const { client, columnDefs, entityName, ...otherProps } = props;
+  const { recursive } = columnDefs;
   const queryName = props.queryName || "findAll";
   const excludedColumns = !props.columnDefs.excludeColumns
     ? []
     : props.columnDefs.excludeColumns;
+  //const [isLoading, setLoading] = useState<boolean>(true);
 
   // Refactored column function as async to ensure column data is fetched before the table is rendered
-  const getColumnsAsync: (entityName: string) => Promise<ColumnProps<T>[]> | undefined = async (entityName: string) => {
+  const getColumnsAsync: (
+    entityName: string
+  ) => Promise<ColumnProps<T>[]> | undefined = async (entityName: string) => {
     const introspectionQuery = buildIntrospectionQuery(entityName);
     const introspectionResult: any = await client.query({
       query: introspectionQuery
@@ -53,31 +64,42 @@ export function ApolloTableView<T extends DomainEntity>(
 
     const fields = introspectionResult.data.__type.fields;
 
-      if (fields) {
+    if (fields) {
+      const cols = fields
+        .filter(
+          (f: any) =>
+            !f.dataIndex || !excludedColumns.includes(f.dataIndex)
+        )
+        .map((c: any) => {
+          let dataIndex =
+            c.dataIndex === columnDefs.keyColumn ? "key" : c.dataIndex;
+            if (recursive && c.type.fields) {
+                c.type.fields.forEach((subField: any) => {
+                    c.children.push({
+                        dataIndex: subField.dataIndex,
+                        key: subField.dataIndex,
+                        title: subField.title || subField.dataIndex
+                    })
+                })
+            }
+          return {
+            dataIndex: dataIndex,
+            key: dataIndex,
+            title:
+                c.title ||
+              (dataIndex === "key" ? columnDefs.keyColumn : dataIndex)
+          };
+        });
 
-          const cols = fields
-              .filter((f: ColumnProps<T>) => !f.dataIndex || !excludedColumns.includes(f.dataIndex))
-              .map((c: ColumnProps<T>) => {
-                  let dataIndex =
-                      c.dataIndex === columnDefs.keyColumn ? "key" : c.dataIndex;
-
-                  return {
-                      dataIndex: dataIndex,
-                      key: dataIndex,
-                      title:
-                          c.title || dataIndex === "key" ? columnDefs.keyColumn : dataIndex
-                  };
-              });
-
-          cols.forEach((c: ColumnProps<T>) => {
-              console.log(`${c.dataIndex} -> ${c.title}`);
-          });
-          // setColumns(cols)
-          return cols;
-      } else {
-          console.log("data doesn't exist");
-          return undefined;
-      }
+      cols.forEach((c: ColumnProps<T>) => {
+        console.log(`${c.dataIndex} -> ${c.title}`);
+      });
+      // setColumns(cols)
+      return cols;
+    } else {
+      console.log("data doesn't exist");
+      return undefined;
+    }
   };
 
   const defaultQuery = () => {
@@ -103,36 +125,44 @@ export function ApolloTableView<T extends DomainEntity>(
     columnDefs.columns && columnDefs.columns
   );
   const [query, setQuery] = useState(props.query || defaultQuery());
+  //const hasQueryChanged = useCompare(query);
 
   //console.log(query);
 
-  const { loading, data } = useQuery(gql(query), { client });
-  console.log(loading, data);
+  const { loading, data }: QueryResult<any> = useQuery(gql(query), {
+    client,
+    skip: !columns
+  });
+  console.log(query, loading, data);
 
   // Awaiting resolution of column query before setting columns/rendering table
   useEffect(() => {
-      const initializeColumns = async () => {
-          const columns = await getColumnsAsync(entityName);
-          setColumns(columns || columnDefs.columns);
-          return columns || columnDefs;
-      };
-      initializeColumns()
+    const initializeColumns = async () => {
+      const columns = await getColumnsAsync(entityName);
+      setColumns(columns || columnDefs.columns);
+      return columns || columnDefs;
+    };
+    initializeColumns();
   }, []);
 
   useEffect(() => {
-    setQuery(props.query || defaultQuery());
+    if (query !== defaultQuery()) {
+      console.log("Columns updated");
+      setQuery(props.query || defaultQuery());
+    }
   }, [columns]);
 
-
-      return (
-          <TableView
-              columns={(columns && query && !loading) ? columns : []}
-              loading={loading || !columns}
-              loadData={() =>
-                  columns && query && !loading ? (data[queryName] as T[]) : []
-              }
-              {...otherProps}
-          >{props.children}</TableView>
-      );
-
+  return (
+    <TableView
+      columns={columns && query && !loading ? columns : []}
+      loading={loading || !columns}
+      loadData={() =>
+        columns && query && !loading ? (data[queryName] as T[]) : []
+      }
+      {...otherProps}
+    >
+        {/*Forward children from UseApolloTableView component*/}
+      {props.children}
+    </TableView>
+  );
 }
--- stories/index.stories.tsx
+++ stories/index.stories.tsx
@@ -1,73 +1,79 @@
-import React from 'react';
-const uuid4 = require('uuid/v4');
+import React from "react";
+const uuid4 = require("uuid/v4");
 
-import {storiesOf} from '@storybook/react';
-import { withKnobs, boolean } from '@storybook/addon-knobs';
+import { storiesOf } from "@storybook/react";
+import { withKnobs, boolean } from "@storybook/addon-knobs";
 
+import { Divider } from "antd";
 
-import {Divider} from "antd";
+import TableView from "../src/tableview/TableView";
+import {
+  InsertTableAction,
+  RefreshTableAction,
+  RemoveTableAction,
+  UpdateTableAction
+} from "../src/tableview/Actions";
 
-import TableView from '../src/tableview/TableView';
-import {InsertTableAction, RefreshTableAction, RemoveTableAction, UpdateTableAction} from "../src/tableview/Actions";
-
-import '../src/indigo.css';
+import "../src/indigo.css";
 import PersonForm from "./PersonForm";
 import Modals from "../src/modal/ModalContaner";
-import {Person} from "./PersonFormik";
-import {Country, CountrySupport} from "./Countries"
-import {useQuery} from "@apollo/react-hooks";
-import {ApolloTableView} from "../src/tableview/ApolloTableView";
+import { Person } from "./PersonFormik";
+import { Country, CountrySupport } from "./Countries";
+import { useQuery } from "@apollo/react-hooks";
+import { ApolloTableView } from "../src/tableview/ApolloTableView";
 
 //TODO derive columns from domain entity
-const columns = [{
-    title: 'First Name',
-    dataIndex: 'firstName',
-    key: 'firstName',
-}, {
-    title: 'Last Name',
-    dataIndex: 'lastName',
-    key: 'lastName',
-}, {
-    title: 'Age',
-    dataIndex: 'age',
-    key: 'age',
-}, {
-    title: 'Profession',
-    dataIndex: 'profession',
-    key: 'profession',
-}];
+const columns = [
+  {
+    title: "First Name",
+    dataIndex: "firstName",
+    key: "firstName"
+  },
+  {
+    title: "Last Name",
+    dataIndex: "lastName",
+    key: "lastName"
+  },
+  {
+    title: "Age",
+    dataIndex: "age",
+    key: "age"
+  },
+  {
+    title: "Profession",
+    dataIndex: "profession",
+    key: "profession"
+  }
+];
 
-function age( bd: Date ): number {
-    let diff =(new Date().getTime() - bd.getTime()) / 1000 / (60 * 60 * 24);
-    return Math.abs(Math.floor(diff/365.25));
+function age(bd: Date): number {
+  let diff = (new Date().getTime() - bd.getTime()) / 1000 / (60 * 60 * 24);
+  return Math.abs(Math.floor(diff / 365.25));
 }
 
 const data: Person[] = [
-    {
-        key:  uuid4(),
-        firstName: "Jason",
-        lastName: "Rocco",
-        age : age( new Date(1971, 9, 15)),
-        profession: 'Director',
-    },
-    {
-        key: uuid4(),
-        firstName: "Roman",
-        lastName: "Vorobiev",
-        age : age( new Date(1966, 7, 7)),
-        profession: 'Software Developer',
-    },
-    {
-        key: uuid4(),
-        firstName: "Vladimir",
-        lastName: "Birbrier",
-        age : age( new Date(1978, 9, 15)),
-        profession: 'Software Developer',
-    },
-
+  {
+    key: uuid4(),
+    firstName: "Jason",
+    lastName: "Rocco",
+    age: age(new Date(1971, 9, 15)),
+    profession: "Director"
+  },
+  {
+    key: uuid4(),
+    firstName: "Roman",
+    lastName: "Vorobiev",
+    age: age(new Date(1966, 7, 7)),
+    profession: "Software Developer"
+  },
+  {
+    key: uuid4(),
+    firstName: "Vladimir",
+    lastName: "Birbrier",
+    age: age(new Date(1978, 9, 15)),
+    profession: "Software Developer"
+  }
 ];
-
-
 
 // storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
@@ -81,220 +87,230 @@ const data: Person[] = [
 //     </Button>
 //   ));
 
-function confirmRemoval( person: Person ): Promise<void> {
-    return Modals.confirm({
-        title: 'Delete selected Item?',
-        content: 'Some descriptions here',
-        okType: 'primary'
-    });
+function confirmRemoval(person: Person): Promise<void> {
+  return Modals.confirm({
+    title: "Delete selected Item?",
+    content: "Some descriptions here",
+    okType: "primary"
+  });
 }
 
+async function insertItem(person?: Person): Promise<Person> {
+  return new Promise<Person>((resolve, reject) => {
+    let newPerson = person
+      ? { ...person, key: uuid4(), firstName: person!.firstName + " +" }
+      : {
+          key: uuid4(),
+          firstName: "Unknown",
+          lastName: "Unknown",
+          age: 0,
+          profession: "Unknown"
+        };
 
-async function insertItem( person?: Person ): Promise<Person> {
-
-    return new Promise<Person>( (resolve, reject) => {
-
-        let newPerson = person ? {...person, key: uuid4(), firstName: person!.firstName + " +"} :
-            {key: uuid4(), firstName: "Unknown", lastName: "Unknown", age: 0, profession: 'Unknown'};
-
-        Modals.show(<PersonForm {...person}/>, {
-            title: 'Insert Person',
-            okText: 'Create',
-            onOk: () =>  resolve(newPerson),
-            onCancel: () => reject(),
-        })
-
+    Modals.show(<PersonForm {...person} />, {
+      title: "Insert Person",
+      okText: "Create",
+      onOk: () => resolve(newPerson),
+      onCancel: () => reject()
     });
+  });
 }
 
-function updateItem( person: Person ): Promise<Person> {
+function updateItem(person: Person): Promise<Person> {
+  return new Promise<Person>((resolve, reject) => {
+    let updatedPerson = {
+      ...person,
+      age: person.age + 10,
+      firstName: person.firstName + " ^"
+    };
 
-    return new Promise<Person>( (resolve,reject) => {
-
-        let updatedPerson = {...person, age: person.age + 10, firstName: person.firstName + " ^"};
-
-        Modals.show( <PersonForm {...person} />,{
-            title   : 'Update Person',
-            okText  : 'Update',
-            onOk    : () => resolve(updatedPerson),
-            onCancel: () => reject(),
-        } )
-
+    Modals.show(<PersonForm {...person} />, {
+      title: "Update Person",
+      okText: "Update",
+      onOk: () => resolve(updatedPerson),
+      onCancel: () => reject()
     });
-
+  });
 }
 
 const retrieveData: () => Person[] = () => data;
 
-const verboseToolbarTitle = 'Verbose Toolbar';
-const multipleSelectionTitle = 'Multiple Selection';
-const disableContextMenuTitle = 'Disable Context Menu';
+const verboseToolbarTitle = "Verbose Toolbar";
+const multipleSelectionTitle = "Multiple Selection";
+const disableContextMenuTitle = "Disable Context Menu";
 
 function UseTableViewWithGraphQL() {
+  const { loading, /*error,*/ data } = useQuery(CountrySupport.query, {
+    client: CountrySupport.client
+  });
 
-    const { loading, /*error,*/ data } = useQuery( CountrySupport.query, {client: CountrySupport.client});
-
-    return (
-        <TableView columns={CountrySupport.columns}
-                   pagination={false}
-                   bordered
-                   scroll={{y: 300}}
-                   loading={loading}
-                   loadData={() => data.countries as Country[]}
-                   verboseToolbar={boolean(verboseToolbarTitle, false)}
-                   multipleSelection={boolean(multipleSelectionTitle, false)}
-                   disableContextMenu={boolean(disableContextMenuTitle, false)}>
-            <RefreshTableAction/>
-            <Divider type="vertical" dashed={true}/>
-            <InsertTableAction onInsert={CountrySupport.insertItem}/>
-            <UpdateTableAction onUpdate={CountrySupport.updateItem}/>
-            <RemoveTableAction onRemove={CountrySupport.confirmRemoval}/>
-        </TableView>
-    )
+  return (
+    <TableView
+      columns={CountrySupport.columns}
+      pagination={false}
+      bordered
+      scroll={{ y: 300 }}
+      loading={loading}
+      loadData={() => data.countries as Country[]}
+      verboseToolbar={boolean(verboseToolbarTitle, false)}
+      multipleSelection={boolean(multipleSelectionTitle, false)}
+      disableContextMenu={boolean(disableContextMenuTitle, false)}
+    >
+      <RefreshTableAction />
+      <Divider type="vertical" dashed={true} />
+      <InsertTableAction onInsert={CountrySupport.insertItem} />
+      <UpdateTableAction onUpdate={CountrySupport.updateItem} />
+      <RemoveTableAction onRemove={CountrySupport.confirmRemoval} />
+    </TableView>
+  );
 }
 
 function UseApolloTableView(props: any) {
-
-    return (
-        <ApolloTableView
-                client={ CountrySupport.client}
-                entityName="Country"
-                queryName="countries"
-                columnDefs={props.columnDefs}
-                query={ props.query}
-                pagination={false}
-                bordered
-                // scroll={{y: 500, x: 1100}}
-                verboseToolbar={boolean(verboseToolbarTitle, false)}
-                multipleSelection={boolean(multipleSelectionTitle, false)}
-                disableContextMenu={boolean(disableContextMenuTitle, false)}>
-            <RefreshTableAction/>
-            <Divider type="vertical" dashed={true}/>
-            <InsertTableAction onInsert={CountrySupport.insertItem}/>
-            <UpdateTableAction onUpdate={CountrySupport.updateItem}/>
-            <RemoveTableAction onRemove={CountrySupport.confirmRemoval}/>
-        </ApolloTableView>
-    )
+  return (
+    <ApolloTableView
+      client={CountrySupport.client}
+      entityName="Country"
+      queryName="countries"
+      columnDefs={props.columnDefs}
+      query={props.query}
+      pagination={false}
+      bordered
+      // scroll={{y: 500, x: 1100}}
+      verboseToolbar={boolean(verboseToolbarTitle, false)}
+      multipleSelection={boolean(multipleSelectionTitle, false)}
+      disableContextMenu={boolean(disableContextMenuTitle, false)}
+    >
+      <RefreshTableAction />
+      <Divider type="vertical" dashed={true} />
+      <InsertTableAction onInsert={CountrySupport.insertItem} />
+      <UpdateTableAction onUpdate={CountrySupport.updateItem} />
+      <RemoveTableAction onRemove={CountrySupport.confirmRemoval} />
+    </ApolloTableView>
+  );
 }
 
-storiesOf('TableView', module)
+storiesOf("TableView", module)
+  .addDecorator(withKnobs)
+  .addDecorator(story => <div style={{ padding: "1rem" }}>{story()}</div>)
 
-    .addDecorator(withKnobs)
-    .addDecorator(story => <div style={{ padding: '1rem' }}>{story()}</div>)
+  .add("with standard toolbar", () => {
+    return (
+      <TableView
+        columns={columns}
+        pagination={false}
+        loadData={retrieveData}
+        verboseToolbar={boolean(verboseToolbarTitle, false)}
+        multipleSelection={boolean(multipleSelectionTitle, false)}
+        disableContextMenu={boolean(disableContextMenuTitle, false)}
+      >
+        <RefreshTableAction />
+        <Divider type="vertical" dashed={true} />
+        <InsertTableAction onInsert={insertItem} />
+        <UpdateTableAction onUpdate={updateItem} />
+        <RemoveTableAction onRemove={confirmRemoval} />
+      </TableView>
+    );
+  })
 
-    .add('with standard toolbar', () => {
-
-        return (
-            <TableView columns={columns}
-                       pagination={false}
-                       loadData={retrieveData}
-                       verboseToolbar={boolean(verboseToolbarTitle, false)}
-                       multipleSelection={boolean(multipleSelectionTitle, false)}
-                       disableContextMenu={boolean(disableContextMenuTitle, false)}>
-                <RefreshTableAction/>
-                <Divider type="vertical" dashed={true}/>
-                <InsertTableAction onInsert={insertItem}/>
-                <UpdateTableAction onUpdate={updateItem}/>
-                <RemoveTableAction onRemove={confirmRemoval}/>
-            </TableView>
-        )
-    })
-
-    .add('with custom toolbar button properties', () => {
-
-        return (
-            <TableView columns={columns} style={{ userSelect: 'none' }}
-                       pagination={false}
-                       loadData={retrieveData}
-                       verboseToolbar={boolean(verboseToolbarTitle, false)}
-                       multipleSelection={boolean(multipleSelectionTitle, false)}
-                       disableContextMenu={boolean(disableContextMenuTitle, false)}>
-                <RefreshTableAction iconProps={{spin:false}}/>
-                <Divider type="vertical" dashed={true}/>
-                <InsertTableAction text={'Create'}
-                                   icon={'plus-circle'}
-                                   type={'primary'}
-                                   shape={'round'}
-                                   onInsert= {insertItem} />
-                <UpdateTableAction onUpdate= {updateItem} type={'dashed' } />
-                <RemoveTableAction onRemove={confirmRemoval} type={'danger'} />
-            </TableView>
-        )
-
-    })
-
-    .add('with custom table properties', () => {
-
-        return (
-            <TableView columns={columns}
-                       pagination={false}
-                       bordered
-                       loadData={retrieveData}
-                       verboseToolbar={boolean(verboseToolbarTitle, false)}
-                       multipleSelection={boolean(multipleSelectionTitle, false)}
-                       disableContextMenu={boolean(disableContextMenuTitle, false)}>
-                <RefreshTableAction />
-                <Divider type="vertical" dashed={true}/>
-                <InsertTableAction onInsert={insertItem} />
-                <UpdateTableAction onUpdate={updateItem} />
-                <RemoveTableAction onRemove={confirmRemoval} />
-            </TableView>
-        )
-
-    })
-
-    .add('with only one action', () => {
-
-        return (
-            <TableView columns={columns}
-                       pagination={false}
-                       bordered
-                       loadData={retrieveData}
-                       verboseToolbar={boolean(verboseToolbarTitle, false)}
-                       multipleSelection={boolean(multipleSelectionTitle, false)}
-                       disableContextMenu={boolean(disableContextMenuTitle, false)}>
-                <RefreshTableAction />
-            </TableView>
-        )
-
-    })
-
-    .add('using Apollo Client', () => {
-        return <UseTableViewWithGraphQL/>
-    })
-
-    .add('using ApolloTableView', () => {
-        return <UseApolloTableView
-            columnDefs={{ keyColumn: "code"}}
-            query="{ countries { key:code }}"
+  .add("with custom toolbar button properties", () => {
+    return (
+      <TableView
+        columns={columns}
+        style={{ userSelect: "none" }}
+        pagination={false}
+        loadData={retrieveData}
+        verboseToolbar={boolean(verboseToolbarTitle, false)}
+        multipleSelection={boolean(multipleSelectionTitle, false)}
+        disableContextMenu={boolean(disableContextMenuTitle, false)}
+      >
+        <RefreshTableAction iconProps={{ spin: false }} />
+        <Divider type="vertical" dashed={true} />
+        <InsertTableAction
+          text={"Create"}
+          icon={"plus-circle"}
+          type={"primary"}
+          shape={"round"}
+          onInsert={insertItem}
         />
-    })
+        <UpdateTableAction onUpdate={updateItem} type={"dashed"} />
+        <RemoveTableAction onRemove={confirmRemoval} type={"danger"} />
+      </TableView>
+    );
+  })
 
-    .add('using ApolloTableView with custom key', () => {
-        return <UseApolloTableView
-            columnDefs={{
-                keyColumn: "code",
-                // columns: [{
-                //     title: 'Code',
-                //     dataIndex: 'key',
-                //     key: 'key',
-                // }],
-            }}
-            query="{ countries { key:code }}"
-            queryName="countries"
-        />
-    })
+  .add("with custom table properties", () => {
+    return (
+      <TableView
+        columns={columns}
+        pagination={false}
+        bordered
+        loadData={retrieveData}
+        verboseToolbar={boolean(verboseToolbarTitle, false)}
+        multipleSelection={boolean(multipleSelectionTitle, false)}
+        disableContextMenu={boolean(disableContextMenuTitle, false)}
+      >
+        <RefreshTableAction />
+        <Divider type="vertical" dashed={true} />
+        <InsertTableAction onInsert={insertItem} />
+        <UpdateTableAction onUpdate={updateItem} />
+        <RemoveTableAction onRemove={confirmRemoval} />
+      </TableView>
+    );
+  })
 
-    .add('using ApolloTableView with exclude columns', () => {
-        return <UseApolloTableView
-            columnDefs={{
-                keyColumn: "code",
-                excludeColumns:["languages","continent"]
-            }}
-            queryName="countries"
-        />
-    })
-    ;
+  .add("with only one action", () => {
+    return (
+      <TableView
+        columns={columns}
+        pagination={false}
+        bordered
+        loadData={retrieveData}
+        verboseToolbar={boolean(verboseToolbarTitle, false)}
+        multipleSelection={boolean(multipleSelectionTitle, false)}
+        disableContextMenu={boolean(disableContextMenuTitle, false)}
+      >
+        <RefreshTableAction />
+      </TableView>
+    );
+  })
 
-;
+  .add("using Apollo Client", () => {
+    return <UseTableViewWithGraphQL />;
+  })
 
+  .add("using ApolloTableView", () => {
+    return (
+      <UseApolloTableView
+        columnDefs={{ keyColumn: "code" }}
+        query="{ countries { key:code }}"
+      />
+    );
+  })
+
+  .add("using ApolloTableView with custom key", () => {
+    return (
+      <UseApolloTableView
+        columnDefs={{
+          keyColumn: "code"
+          // columns: [{
+          //     title: 'Code',
+          //     dataIndex: 'key',
+          //     key: 'key',
+          // }],
+        }}
+        query="{ countries { key:code }}"
+        queryName="countries"
+      />
+    );
+  })
+
+  .add("using ApolloTableView with exclude columns", () => {
+    return (
+      <UseApolloTableView
+        columnDefs={{
+          keyColumn: "code",
+        }}
+        queryName="countries"
+      />
+    );
+  });
